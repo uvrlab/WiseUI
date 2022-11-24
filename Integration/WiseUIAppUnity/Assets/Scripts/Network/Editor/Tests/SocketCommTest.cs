fcdfa518cce58671779d516f9937b0218046726e
ikbeomjeon <jeonikbeom@gmail.com>
2022-11-24 16:27:28 +0900

    ..

--- Integration/WiseUIAppUnity/Assets/Scripts/Network/Editor/Tests/SocketCommTest.cs
+++ Integration/WiseUIAppUnity/Assets/Scripts/Network/Editor/Tests/SocketCommTest.cs
@@ -52,7 +52,7 @@ public class SocketCommTest
             clients[i].SendRGBImage(i, new Texture2D(320, 240, TextureFormat.RGB24, false), ImageCompression.None);
             Thread.Sleep(10);
         }
-        Thread.Sleep(10000);
+        Thread.Sleep(100);
         Assert.AreEqual(num_client, list_received_buffer.Count);
 
         foreach (var buffer in list_received_buffer)
